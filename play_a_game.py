8efd8dae43772bd4527b205dcaa7a2065fb41022
De Kocker Camille <camille.dekocker@ensta-paristech.fr>
2019-05-24 17:55:41 +0200

    fichier super stylé qui permet de faire jouer nos deux IA

--- play_a_game.py
+++ play_a_game.py
@@ -2,20 +2,50 @@ import hanabi
 import hanabi.ai
 import hanabi.ai_recom
 
-#nom_de_IA = 'Random' #Use Random strat in ai.py file"
-nom_de_IA = 'Recom' #Use Recom strat in ai_recom.py file"
 
-nb_players = 4 #à modifier
+nom_de_IA = input("quelle IA voulez-vous tester ? [Random/Recom] :")
+nb_players = int(input("combien de joueurs ? [2/3/4/5]:"))
+mode = input("lancer toute la partie [R] ou jouer tour par tour [T] ? :")
 
-if nom_de_IA == 'Random' :
-    game = hanabi.Game(nb_players)
-    ai = hanabi.ai.Random(game)
-    game.ai = ai
-    game.run()
+while nom_de_IA not in ['Random','Recom'] :
+    nom_de_IA = input("quelle IA voulez-vous tester ? [Random/Recom] :")
 
-if nom_de_IA == 'Recom' :
-    game = hanabi.Game(nb_players)
-    ai = hanabi.ai_recom.Recom_Strategist_2(game)
-    game.ai = ai
+while nb_players > 5 or nb_players <2 :
+    nb_players = input("combien de joueurs ? [2/3/4/5]:")
+
+while mode not in 'RT' :
+    mode = input("lancer toute la partie [R] ou jouer tour par tour [T] ? :")
+
+if mode == 'R' : 
+    if nom_de_IA == 'Random' :
+        game = hanabi.Game(nb_players)
+        ai = hanabi.ai.Random(game)
+        game.ai = ai
+        game.run()
+
+    if nom_de_IA == 'Recom' :
+        game = hanabi.Game(nb_players)
+        ai = hanabi.ai_recom.Recom_Strategist_2(game)
+        game.ai = ai
+        game.run()
+
+if mode == 'T' :
+    
+    if nom_de_IA == 'Random' :
+            game = hanabi.Game(nb_players)
+            ai = hanabi.ai.Random(game)
+            game.ai = ai
+    
+    if nom_de_IA == 'Recom' :
+            game = hanabi.Game(nb_players)
+            ai = hanabi.ai_recom.Recom_Strategist_2(game)
+            game.ai = ai
+    
+    Turn = True 
+    
+    while Turn is True :
+        game.turn(ai)
+        Turn = input("continuer à jouer [True] ou arrêter et finir la partie [False] ? :")
+    
     game.run()
 
