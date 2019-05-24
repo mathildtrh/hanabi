49d15a8bbd22e0af55eef06866b38bf3aca911b9
De Kocker Camille <camille.dekocker@ensta-paristech.fr>
2019-05-24 11:20:04 +0200

    add some pics and update files

--- test/script_comparison.py
+++ test/script_comparison.py
@@ -7,10 +7,10 @@ import hanabi.ai_recom
 
 ai1 = hanabi.ai_recom.Recom_Strategist_2()
 ai2 = hanabi.ai.Random()
-ai3 = rec1.Recom_Strategist()
+#ai3 = rec1.Recom_Strategist()
 
 a,b = c.compare([ai1, ai2], 1000)
-c.affichage_alone(b[0], a[0], "AI named Recom")
+c.affichage_alone(b[0], a[0], "AI named Recommandation")
 c.affichage_alone(b[1], a[1], "AI named Random")
-c.affichage_alone(b[1], a[1], "AI that plays with a recommandation strategy")
+#c.affichage_alone(b[1], a[1], "AI that plays with a recommandation strategy")
 c.affichage_general(b,a)
